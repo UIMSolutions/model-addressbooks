350bf98617389f4def941a83cb3245dfe415504e
UI-Manufaktur <ui.manufaktur@gmail.com>
2022-07-22 09:47:48 +0200

    Update CompanyInfo

--- source/models/addressbooks/entities/companyinfo.d
+++ source/models/addressbooks/entities/companyinfo.d
@@ -1,2 +1,32 @@
 module models.addressbooks.entities.companyinfo;
 
+@safe:
+import models.applications;
+
+class DCompanyInfoEntity : DOOPEntity {
+  mixin(EntityThis!("CompanyInfoEntity"));
+  
+  override void initialize() {
+    super.initialize;
+
+    this
+      .addValues([ // individual values
+        "KnownAs": StringAttribute, 
+        "LanguageId": StringAttribute, 
+        "Name": StringAttribute, 
+        "PartyNumber": StringAttribute, 
+        "DataArea": StringAttribute, 
+        "BackingTable_CompanyInfoRelationshipId": StringAttribute, 
+      ])
+      .registerPath("addressbooks_companyinfos");
+  }
+}
+mixin(EntityCalls!("CompanyInfoEntity"));
+
+version(test_library) {
+  unittest {
+    assert(CompanyInfoEntity);
+  
+    auto entity = CompanyInfoEntity;
+  }
+}
