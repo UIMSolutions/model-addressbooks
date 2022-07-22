5205390192249027d27d4881f79faef74851962d
UI-Manufaktur <ui.manufaktur@gmail.com>
2022-07-22 16:38:30 +0200

    Update

--- source/models/addressbooks/entities/dir/externalrole.d
+++ source/models/addressbooks/entities/dir/externalrole.d
@@ -1,2 +1,29 @@
-module models.addressbooks.entities.dir.direxternalrole;
+module models.addressbooks.entities.dir.externalrole;
 
+@safe:
+import models.addressbooks;
+
+class DDirExternalRoleEntity : DOOPEntity {
+  mixin(EntityThis!("DirExternalRoleEntity"));
+  
+  override void initialize() {
+    super.initialize;
+
+    this
+      .addValues([ // individual values
+        "Role": StringAttribute,
+        "Type":	StringAttribute,
+        "BackingTable_DirExternalRoleRelationshipId": StringAttribute,
+      ])
+      .registerPath("addressbooks_dir.externalrole");
+  }
+}
+mixin(EntityCalls!("DirExternalRoleEntity"));
+
+version(test_library) {
+  unittest {
+    assert(DirExternalRoleEntity);
+  
+    auto entity = DirExternalRoleEntity;
+  }
+}
