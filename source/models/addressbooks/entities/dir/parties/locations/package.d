665be267bad55d74945ccbf29c7d996574e563b4
UIM (powered by Sicherheitsschmiede) <ui.manufaktur@gmail.com>
2022-07-24 19:26:20 +0200

    Update package.d

--- source/models/addressbooks/entities/dir/parties/locations/package.d
+++ source/models/addressbooks/entities/dir/parties/locations/package.d
@@ -1,2 +1,4 @@
 module models.addressbooks.entities.dir.parties.locations;
 
+public import models.addressbooks.entities.dir.parties.locations.postaladdress;
+public import models.addressbooks.entities.dir.parties.locations.postaladdresshistorical;
