4eddf9f11dc1586bc5761898c135885d26ec80f8
UIM (powered by Sicherheitsschmiede) <ui.manufaktur@gmail.com>
2022-07-24 19:22:35 +0200

    Update package.d

--- source/models/addressbooks/entities/dir/package.d
+++ source/models/addressbooks/entities/dir/package.d
@@ -1,2 +1,10 @@
 module models.addressbooks.entities.dir;
 
+public import models.addressbooks.entities.dir.adressbook;
+public import models.addressbooks.entities.dir.externalrole;
+public import models.addressbooks.entities.dir.nameaffix;
+public import models.addressbooks.entities.dir.namesequence;
+public import models.addressbooks.entities.dir.parameter;
+public import models.addressbooks.entities.dir.partybase;
+public import models.addressbooks.entities.dir.partycontact;
+public import models.addressbooks.entities.dir.partylocationpostaladress;
