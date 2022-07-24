27bb60a62e455bc0fc026d438e3394a767fb12a7
UIM (powered by Sicherheitsschmiede) <ui.manufaktur@gmail.com>
2022-07-24 19:15:03 +0200

    Update package.d

--- source/models/addressbooks/package.d
+++ source/models/addressbooks/package.d
@@ -1,4 +1,6 @@
 module models.addressbooks;
 
 public import models.addressbooks.attributes;
+public import models.addressbooks.classes;
+public import models.addressbooks.elememts;
 public import models.addressbooks.entities;
