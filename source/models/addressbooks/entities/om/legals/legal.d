e05882eca0ce51951f12958a6216fbb3f7293354
UIM (powered by Sicherheitsschmiede) <ui.manufaktur@gmail.com>
2022-07-30 09:16:13 +0200

    Update legal.d

--- source/models/addressbooks/entities/om/legals/legal.d
+++ source/models/addressbooks/entities/om/legals/legal.d
@@ -1,1 +1,29 @@
+module models.addressbooks.entities.om.businessunit;
 
+@safe:
+import models.addressbooks;
+
+class DDirNameAffixEntity : DOOPEntity {
+  mixin(EntityThis!("DirNameAffixEntity"));
+  
+  override void initialize() {
+    super.initialize;
+
+    this
+      .addValues([ // individual values
+        "Affix":	StringAttribute,
+        "Type":	StringAttribute,
+        "BackingTable_DirNameAffixRelationshipId":	StringAttribute,
+      ])
+      .registerPath("addressbooks_dir.nameaffixes");
+  }
+}
+mixin(EntityCalls!("DirNameAffixEntity"));
+
+version(test_library) {
+  unittest {
+    assert(DirNameAffixEntity);
+  
+    auto entity = DirNameAffixEntity;
+  }
+}
