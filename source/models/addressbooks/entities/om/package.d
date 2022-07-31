30c6cfac1e49449ce242b6ce6ab5abdc4af75f77
UI-Manufaktur <ui.manufaktur@gmail.com>
2022-07-31 16:32:27 +0200

    Packgaes in models.addressbooks.entities.om

--- source/models/addressbooks/entities/om/package.d
+++ source/models/addressbooks/entities/om/package.d
@@ -1,2 +1,12 @@
 module models.addressbooks.entities.om;
 
+// Packages
+public import models.addressbooks.entities.om.legals;
+public import models.addressbooks.entities.om.operatingunits;
+public import models.addressbooks.entities.om.organizationhierarchies;
+public import models.addressbooks.entities.om.teams;
+
+// Modules
+public import models.addressbooks.entities.om.businessunit;
+public import models.addressbooks.entities.om.costcenter;
+public import models.addressbooks.entities.om.department;
