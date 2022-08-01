d4fa66088396059a21f1bf5d43a633b79d448752
UIM (powered by Sicherheitsschmiede) <ui.manufaktur@gmail.com>
2022-08-01 06:40:28 +0200

    Update stead.d

--- source/models/addressbooks/entities/fias/stead.d
+++ source/models/addressbooks/entities/fias/stead.d
@@ -4,26 +4,37 @@ module models.addressbooks.entities.fias.stead;
 import models.addressbooks;
 
 class DDirNameAffixEntity : DOOPEntity {
-  mixin(EntityThis!("DirNameAffixEntity"));
+  mixin(EntityThis!("FiasSteadEntity"));
   
   override void initialize() {
     super.initialize;
 
     this
       .addValues([ // individual values
-        "Affix":	StringAttribute,
-        "Type":	StringAttribute,
-        "BackingTable_DirNameAffixRelationshipId":	StringAttribute,
+steadId		GAB/FiasSteadEntity
+steadGuid		GAB/FiasSteadEntity
+parentGuid		GAB/FiasSteadEntity
+regionCode		GAB/FiasSteadEntity
+divType		GAB/FiasSteadEntity
+number		GAB/FiasSteadEntity
+postalCode		GAB/FiasSteadEntity
+ifnsfl		GAB/FiasSteadEntity
+terrIFNSFL		GAB/FiasSteadEntity
+okato		GAB/FiasSteadEntity
+startDate		GAB/FiasSteadEntity
+endDate		GAB/FiasSteadEntity
+backingTable_FiasSteadRelationshipId		GAB/FiasSteadEntity
+relationship_PrimaryCompanyContextRelationshipId		GAB/FiasSteadEntity
       ])
       .registerPath("addressbooks_dir.nameaffixes");
   }
 }
-mixin(EntityCalls!("DirNameAffixEntity"));
+mixin(EntityCalls!("FiasSteadEntity"));
 
 version(test_library) {
   unittest {
-    assert(DirNameAffixEntity);
+    assert(FiasSteadEntity);
   
-    auto entity = DirNameAffixEntity;
+    auto entity = FiasSteadEntity;
   }
 }
