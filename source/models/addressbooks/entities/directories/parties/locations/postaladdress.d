acc68c1dd64dcfab5b5593cafe9b619c25e28bc3
UIM (powered by Sicherheitsschmiede) <ui.manufaktur@gmail.com>
2022-08-16 06:53:47 +0200

    Working on DirectoryPartyLocationPostalAddressEntity

--- source/models/addressbooks/entities/directories/parties/locations/postaladdress.d
+++ source/models/addressbooks/entities/directories/parties/locations/postaladdress.d
@@ -3,25 +3,66 @@ module models.addressbooks.entities.directories.parties.locations.postaladdress;
 @safe:
 import models.addressbooks;
 
-class DDirAddressBooksEntity : DOOPEntity {
-  mixin(EntityThis!("DirAddressBooksEntity"));
+class DDirPartyLocationPostalAddressEntity : DOOPEntity {
+  mixin(EntityThis!("DirPartyLocationPostalAddressEntity"));
   
   override void initialize() {
     super.initialize;
 
     this
       .addValues([ // individual values
-...
+        "attentionToAddressLine": StringAttribute,
+        "isLocationOwner": StringAttribute,
+        "isPostalAddress": StringAttribute,
+        "isPrimary": StringAttribute,
+        "isPrimaryTaxRegistration": StringAttribute,
+        "isPrivate": StringAttribute,
+        "isRoleBusiness": StringAttribute,
+        "isRoleDelivery": StringAttribute,
+        "isRoleHome": StringAttribute,
+        "isRoleInvoice": StringAttribute,
+        "location": StringAttribute,
+        "party": StringAttribute,
+        "partyNumber": StringAttribute,
+        "address": StringAttribute,
+        "apartment_RU": StringAttribute,
+        "building_RU": StringAttribute,
+        "buildingCompliment": StringAttribute,
+        "city": StringAttribute,
+        "cityInKana": StringAttribute,
+        "countryRegionId": StringAttribute,
+        "countryRegionISOCode": StringAttribute,
+        "county": StringAttribute,
+        "description": StringAttribute,
+        "districtName": StringAttribute,
+        "dunsNumber": StringAttribute,
+        "isPrivatePostalAddress": StringAttribute,
+        "latitude": StringAttribute,
+        "locationId": StringAttribute,
+        "longitude": StringAttribute,
+        "postBox": StringAttribute,
+        "state": StringAttribute,
+        "street": StringAttribute,
+        "streetInKana": StringAttribute,
+        "streetNumber": StringAttribute,
+        "timeZone": StringAttribute,
+        "validFrom": StringAttribute,
+        "validTo": StringAttribute,
+        "zipCode": StringAttribute,
+        "roles": StringAttribute,
+        "resolveDuplicateAddress": StringAttribute,
+        "taxGroup": StringAttribute,
+        "backingTable_LogisticsPostalAddressBaseEntityRelationshipId": StringAttribute,
       ])
       .registerPath("addressbooks_directories.addressbook");
   }
 }
-mixin(EntityCalls!("DirAddressBooksEntity"));
+mixin(EntityCalls!("DirPartyLocationPostalAddressEntity"));
 
 version(test_library) {
   unittest {
-    assert(DirAddressBooksEntity);
+    assert(DirPartyLocationPostalAddressEntity);
   
-    auto entity = DirAddressBooksEntity;
+    auto entity = DirPartyLocationPostalAddressEntity;
   }
 }
