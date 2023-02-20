1b3012bc189043b6d19792cd24828af04360ab07
UIM GIT-Master <ui.manufaktur@gmail.com>
2023-02-20 15:47:55 +0100

    Enhance call initialize (DConfigurationValue)

--- source/models/addressbooks/entities/om/teams/type.d
+++ source/models/addressbooks/entities/om/teams/type.d
@@ -6,8 +6,8 @@ import models.addressbooks;
 class DOMTeamTypeEntity : DOOPEntity {
   mixin(EntityThis!("OMTeamTypeEntity"));
   
-  override void initialize() {
-    super.initialize;
+  override void initialize(DConfigurationValue configSettings = null) {
+    super.initialize(configSettings);
 
     this
       .addValues([ // individual values
