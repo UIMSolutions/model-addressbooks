924957b16b36d5db8625502a8bcabad77cfbceb5
UIM (powered by Sicherheitsschmiede) <ui.manufaktur@gmail.com>
2023-03-25 07:47:19 +0100

    Update package.d

--- source/models/addressbooks/entities/legals/package.d
+++ source/models/addressbooks/entities/legals/package.d
@@ -1,2 +1,6 @@
 module models.addressbooks.entities.legals;
 
+public {
+  import models.addressbooks.entities.legals.representative;
+  import models.addressbooks.entities.legals.representativecontact;
+}
